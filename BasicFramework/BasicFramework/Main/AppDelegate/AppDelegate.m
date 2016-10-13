b02456b9165bdf5abda56f09d331fae7f386709a
磊 <506466852@qq.com>
2016-10-13 12:33:38 +0800

    手势密码1
    手势密码1

--- BasicFramework/BasicFramework/Main/AppDelegate/AppDelegate.m
+++ BasicFramework/BasicFramework/Main/AppDelegate/AppDelegate.m
@@ -15,6 +15,7 @@
 #import <UMSocialCore/UMSocialCore.h>
 #import "WXApi.h"
 #import "WXApiManager.h"
+#import <AlipaySDK/AlipaySDK.h>
 @interface AppDelegate ()
 
 @end
@@ -24,7 +25,21 @@
 -(BOOL)backBlockWithUrl:(NSURL *)url{
     BOOL result = [[UMSocialManager defaultManager] handleOpenURL:url];
     NSLog(@"%@",url.absoluteString);
-    
+    //如果极简开发包不可用，会跳转支付宝钱包进行支付，需要将支付宝钱包的支付结果回传给开发包
+    if ([url.host isEqualToString:@"safepay"]) {
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            //【由于在跳转支付宝客户端支付的过程中，商户app在后台很可能被系统kill了，所以pay接口的callback就会失效，请商户对standbyCallback返回的回调结果进行处理,就是在这个方法里面处理跟callback一样的逻辑】
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    if ([url.host isEqualToString:@"platformapi"]){//支付宝钱包快登授权返回authCode
+        
+        [[AlipaySDK defaultService] processAuthResult:url standbyCallback:^(NSDictionary *resultDic) {
+            //【由于在跳转支付宝客户端支付的过程中，商户app在后台很可能被系统kill了，所以pay接口的callback就会失效，请商户对standbyCallback返回的回调结果进行处理,就是在这个方法里面处理跟callback一样的逻辑】
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+
     
     if (!result) {
         // 其他如支付宝支付等SDK的回调
---
+++ BasicFramework/BasicFramework/Main/AppDelegate/GestureTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  GestureTestViewController.m
+//  BasicFramework
+//
+//  Created by 周航 on 16/10/13.
+//  Copyright © 2016年 Rainy. All rights reserved.
+//
+
+#import "GestureTestViewController.h"
+
+@interface GestureTestViewController ()
+
+@end
+
+@implementation GestureTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
